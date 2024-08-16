7ba002a34412350bc6d0baa84d55cdc22d5ce055
Alan Silva <alanclash2541@gmail.com>
2024-08-16 00:37:15 -0300

    feat: add customers when starting the application

--- src/main/java/dev/alancss/Main.java
+++ src/main/java/dev/alancss/Main.java
@@ -1,7 +1,13 @@
 package dev.alancss;
 
+import dev.alancss.customer.Customer;
+import dev.alancss.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
@@ -10,4 +16,22 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+        return args -> {
+            var customer = new Customer(
+                    "customer",
+                    "customer@mail.com",
+                    20
+            );
+
+            var user = new Customer(
+                    "user",
+                    "user@mail.com",
+                    19
+            );
+
+            customerRepository.saveAll(List.of(customer, user));
+        };
+    }
 }

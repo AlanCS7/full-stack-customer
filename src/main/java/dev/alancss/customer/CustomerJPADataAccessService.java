14d5226930423d5f714c2ede5fadfa92ecaf02b0
Alan Silva <alanclash2541@gmail.com>
2024-08-16 00:25:01 -0300

    feat: implement CustomerJPADataAccessService

---
+++ src/main/java/dev/alancss/customer/CustomerJPADataAccessService.java
@@ -0,0 +1,26 @@
+package dev.alancss.customer;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CustomerJPADataAccessService implements CustomerDao {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerJPADataAccessService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> findById(Integer id) {
+        return customerRepository.findById(id);
+    }
+}

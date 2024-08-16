6a0a5697a8d98a3d7a4f52da01a54ece7b025868
Alan Silva <alanclash2541@gmail.com>
2024-08-15 23:48:50 -0300

    feat: create fake customer database

---
+++ src/main/java/dev/alancss/customer/CustomerDao.java
@@ -0,0 +1,12 @@
+package dev.alancss.customer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerDao {
+
+    List<Customer> findAll();
+
+    Optional<Customer> findById(Integer id);
+
+}
---
+++ src/main/java/dev/alancss/customer/CustomerDataAccessService.java
@@ -0,0 +1,45 @@
+package dev.alancss.customer;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CustomerDataAccessService implements CustomerDao {
+
+    private static final List<Customer> customers;
+
+    static {
+        customers = new ArrayList<>();
+
+        var customer = new Customer(
+                1,
+                "customer",
+                "customer@mail.com",
+                20
+        );
+        customers.add(customer);
+
+        var user = new Customer(
+                2,
+                "user",
+                "user@mail.com",
+                19
+        );
+        customers.add(user);
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return customers;
+    }
+
+    @Override
+    public Optional<Customer> findById(Integer id) {
+        return customers.stream()
+                .filter(customer -> customer.getId().equals(id))
+                .findFirst();
+    }
+}

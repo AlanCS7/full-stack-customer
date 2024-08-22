2d67a9a500d66e506fffadc2e340338ba9e9ecdd
Alan Silva <alanclash2541@gmail.com>
2024-08-22 01:17:50 -0300

    feat: add flyway and create first migrate

--- src/main/java/dev/alancss/customer/Customer.java
+++ src/main/java/dev/alancss/customer/Customer.java
@@ -9,12 +9,13 @@ public class Customer {
 
     @Id
     @SequenceGenerator(
-            name = "customer_id_sequence",
-            sequenceName = "customer_id_sequence"
+            name = "customer_id_seq",
+            sequenceName = "customer_id_seq",
+            allocationSize = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
-            generator = "customer_id_sequence"
+            generator = "customer_id_seq"
     )
     private Integer id;
 

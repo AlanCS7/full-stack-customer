fbc91c64999bddb227edb4a8ca528c51b2679649
Alan Silva <alanclash2541@gmail.com>
2024-08-22 23:59:52 -0300

    feat: use java faker library to insert new random customer on database

--- src/main/java/dev/alancss/Main.java
+++ src/main/java/dev/alancss/Main.java
@@ -1,5 +1,6 @@
 package dev.alancss;
 
+import com.github.javafaker.Faker;
 import dev.alancss.customer.Customer;
 import dev.alancss.customer.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -7,7 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
+import java.util.Random;
 
 @SpringBootApplication
 public class Main {
@@ -19,19 +20,18 @@ public class Main {
     @Bean
     CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
         return args -> {
+            var faker = new Faker();
+            var firstName = faker.name().firstName();
+            var lastName = faker.name().lastName();
+            var random = new Random();
+
             var customer = new Customer(
-                    "customer",
-                    "customer@mail.com",
-                    20
+                    firstName + " " + lastName,
+                    firstName.toLowerCase() + "." + lastName.toLowerCase() + "@mail.com",
+                    random.nextInt(16, 99)
             );
 
-            var user = new Customer(
-                    "user",
-                    "user@mail.com",
-                    19
-            );
-
-            customerRepository.saveAll(List.of(customer, user));
+            customerRepository.save(customer);
         };
     }
 }

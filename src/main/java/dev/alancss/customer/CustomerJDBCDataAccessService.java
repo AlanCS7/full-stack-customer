c19e781b8d40358318cf916d85cd5146308997f8
Alan Silva <alanclash2541@gmail.com>
2024-08-23 00:06:57 -0300

    feat: create CustomerJDBCDataAccessService to use JdbcTemplate

---
+++ src/main/java/dev/alancss/customer/CustomerJDBCDataAccessService.java
@@ -0,0 +1,52 @@
+package dev.alancss.customer;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("jdbc")
+public class CustomerJDBCDataAccessService implements CustomerDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public CustomerJDBCDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Customer> findById(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void insert(Customer customer) {
+
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return false;
+    }
+
+    @Override
+    public boolean existsById(Integer id) {
+        return false;
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public void update(Customer customer) {
+
+    }
+}

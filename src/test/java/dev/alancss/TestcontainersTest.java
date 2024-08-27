f1fe5fd5df72f2d3ba9bc9b5ec34564d8dbeb797
Alan Silva <alanclash2541@gmail.com>
2024-08-26 22:07:58 -0300

    test: add testcontainers and test if it works

---
+++ src/test/java/dev/alancss/TestcontainersTest.java
@@ -0,0 +1,28 @@
+package dev.alancss;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public class TestcontainersTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer;
+
+    static {
+        postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
+                .withDatabaseName("customer-dao-unit-test")
+                .withUsername("username")
+                .withPassword("password");
+    }
+
+    @Test
+    void canStartPostgresDB() {
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+    }
+}

4134b9b589d81b76ac1793a3db54f1482a5454bb
Alan Silva <alanclash2541@gmail.com>
2024-11-03 22:09:20 -0300

    feat: create sidebar with header

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,5 +1,14 @@
-function App() {
-  return <h1>Hello React</h1>;
-}
+import { Button } from "@chakra-ui/react";
+import SidebarWithHeader from "./shared/SideBar";
+
+const App = () => {
+  return (
+    <SidebarWithHeader>
+      <Button colorScheme="teal" variant="outline">
+        Click me
+      </Button>
+    </SidebarWithHeader>
+  );
+};
 
 export default App;
